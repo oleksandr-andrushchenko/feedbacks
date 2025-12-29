112c1fc2b699f24dd3d000bec35bebef8efc6baf
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-12-28 22:51:36 -0700

    dynamodb: test setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,6 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     $dotenv = new Dotenv();
     $dotenv->loadEnv(dirname(__DIR__) . '/.env');
-    $dotenv->populate(['APP_ENV' => 'test', 'APP_DEBUG' => true], true);
 }
 
 if ($_SERVER['APP_DEBUG']) {
